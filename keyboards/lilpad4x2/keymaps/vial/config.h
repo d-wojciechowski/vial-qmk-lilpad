4f5b252c0e1ca013576fceac69a0d005c8f39b16
Dominik Wojciechowski <doominikwoojciechowski@gmail.com>
2025-04-15 19:31:38 +0200

    vial

---
+++ keyboards/lilpad4x2/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x30, 0x9F, 0x22, 0x10, 0xA2, 0x65, 0x3D, 0xA6}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
